18d3b64582dd1dcc5f6657e25974a10e9237b3e8
micheleF17 <71287675+micheleF17@users.noreply.github.com>
2020-11-30 09:46:08 +0100

    Update Giocatore.java

--- Parte 2/Grenzi-Frisoli/src/Giocatore.java
+++ Parte 2/Grenzi-Frisoli/src/Giocatore.java
@@ -1,4 +1,18 @@
-
 public class Giocatore {
+	private int posizione = 0;
+	private String nomeGiocatore;
+	private String colore;
+	
+	public Giocatore(String nomeGiocatore, String colore) {
+		this.nomeGiocatore = nomeGiocatore;
+		this.colore = colore;
+	}
+	
+	public int muovi() {
+		Dado valore = new Dado();
+		posizione = posizione + valore.lancia();
+		return posizione;
+	}
 
 }
+
