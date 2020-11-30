65ba1524f7f53967e550ee9b78e7683552ef32a6
micheleF17 <71287675+micheleF17@users.noreply.github.com>
2020-11-30 09:45:37 +0100

    Update Dado.java

--- Parte 2/Grenzi-Frisoli/src/Dado.java
+++ Parte 2/Grenzi-Frisoli/src/Dado.java
@@ -1,4 +1,13 @@
 
-public class Dado {
+import java.util.Random;
 
+public class Dado {
+	private int numero_casuale;
+	
+	public int lancia() {
+		Random random = new Random();
+		numero_casuale = random.nextInt(6);	
+		numero_casuale = numero_casuale + 1;
+		return numero_casuale;
+		}
 }
